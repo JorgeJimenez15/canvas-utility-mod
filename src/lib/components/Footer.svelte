168cad4abc06c7e34c9345dc693ecf8733af076d
Jorge Jiménez <github@jorgejimenez.net>
2024-08-06 15:00:25 -0600

    Create footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer class="flex flex-col items-center gap-1 bg-gray-200 p-2 text-sm">
+	<ul class="flex gap-2">
+		<li>
+			<a
+				href="https://github.com/JorgeJimenez15/canvas-utility-mod"
+				target="_blank"
+				class="underline transition-colors hover:text-red-400 focus:text-red-400">GitHub</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://discord.gg/nStnDTjucy"
+				target="_blank"
+				class="underline transition-colors hover:text-red-400 focus:text-red-400">Discord</a
+			>
+		</li>
+	</ul>
+	<span class="text-xs">canvas-utility-mod {VERSION} ({import.meta.env.MODE})</span>
+</footer>
