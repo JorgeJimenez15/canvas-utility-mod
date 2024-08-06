067b1964f20117f6a4e8c1aefc90de0e7b225baa
Jorge Jiménez <github@jorgejimenez.net>
2024-08-06 15:00:16 -0600

    Create header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const { display_name, avatar_image_url } = window.ENV.current_user;
+</script>
+
+<header class="flex h-16 items-center justify-between bg-red-400 text-gray-100">
+	<h1 class="ml-4 text-2xl font-bold">Canvas Utility Mod</h1>
+	<div class="pointer-events-none flex items-center gap-4">
+		<div>
+			<span class="font-semibold">{display_name}</span>
+		</div>
+		<img src={avatar_image_url} alt="Avatar" class="size-16 bg-white" />
+	</div>
+</header>
