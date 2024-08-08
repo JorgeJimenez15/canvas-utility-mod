a89d7a02eb87ba59d930ec29617b4983fdeb9a63
Jorge Jiménez <github@jorgejimenez.net>
2024-08-08 11:45:23 -0600

    Update main app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import patch from './lib/functions/patch';
+import Toastify from 'toastify-js';
 import { mount } from 'svelte';
 
+import patch from './lib/functions/patch';
 import App from './App.svelte';
 import './app.css';
 
@@ -10,4 +11,11 @@ window.addEventListener('load', () => {
 	mount(App, {
 		target: document.body
 	});
+
+	Toastify({
+		text: 'Welcome to Canvas Utility Mod',
+		gravity: 'bottom',
+		position: 'right',
+		className: 'twp'
+	}).showToast();
 });
