708a8dcaed0cd6871d785f3e42b778ecffbfafa1
Jorge Jiménez <github@jorgejimenez.net>
2024-08-17 13:37:50 -0600

    Update config utilities functions

--- src/lib/functions/utils/setConfig.ts
+++ src/lib/functions/utils/setConfig.ts
@@ -2,6 +2,7 @@ import type { Config } from '../config';
 
 const setConfig = (config: Config): void => {
 	GM_setValue('config', JSON.stringify(config));
+	console.log(JSON.stringify(config));
 };
 
 export default setConfig;
