3866a92f293c52fba781e7fd44d1be5f3f806499
Jorge Jiménez <github@jorgejimenez.net>
2024-08-17 13:39:46 -0600

    Remove console log in setConfig

--- src/lib/functions/utils/setConfig.ts
+++ src/lib/functions/utils/setConfig.ts
@@ -2,7 +2,6 @@ import type { Config } from '../config';
 
 const setConfig = (config: Config): void => {
 	GM_setValue('config', JSON.stringify(config));
-	console.log(JSON.stringify(config));
 };
 
 export default setConfig;
